4280ade02d23ddc649023eda5284aff3c60e8b1e
amellalalex <github@alexamellal.com>
2022-07-03 10:59:30 -0300

    continued support for basic features

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod s3support;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bucket = s3support::connect().await?;
     s3support::list(&bucket).await?;
+    s3support::download_file(&bucket, "monke.gif", "LEmonke.gif").await?;
 
     Ok(())
 }
--- src/s3support.rs
+++ src/s3support.rs
@@ -8,6 +8,9 @@
 // }
 
 use s3::{creds::Credentials, Bucket, Region};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 type S3DefaultResult = Result<(), Box<dyn std::error::Error>>;
 type S3BucketResult = Result<Bucket, Box<dyn std::error::Error>>;
@@ -28,13 +31,22 @@ pub async fn connect() -> Result<Bucket, Box<dyn std::error::Error>>  {
     Ok(bucket)
 }
 
-pub async fn upload_file(local_path: &str, remote_path: &str) -> S3DefaultResult {
+pub async fn upload_file(bucket: &Bucket, local_path: &str, remote_path: &str) -> S3DefaultResult {
+    #[cfg(feature = "with-tokio")]
+    let mut file = tokio::fs::File::open(local_path).await?;
+   
+    #[cfg(feature = "with-async-std")]
+    let code = bucket.put_object_stream(&mut file, remote_path).await?;
+    
 
     Ok(())
 }
 
 pub async fn download_file(bucket: &Bucket, remote_path: &str, local_path: &str) -> S3DefaultResult {
     let (data, code) = bucket.get_object(remote_path).await?;
+
+    let mut file = File::create(local_path)?;
+    file.write_all(&data)?;
 
     Ok(())
 }
