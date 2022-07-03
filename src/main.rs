e25e90065e08cdc59203d94854650d8e4d11d06a
amellalalex <github@alexamellal.com>
2022-07-02 22:17:10 -0300

    created s3support module

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,7 @@
-use s3::{creds::Credentials, Bucket, Region};
+mod s3support;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    connect_to_bucket().await?;
+    s3support::connect_to_bucket().await?;
     Ok(())
-}
-
-async fn connect_to_bucket() -> Result<(), Box<dyn std::error::Error>>  {
-    let region_name = "us-west-004".to_string();
-    let endpoint = "https://s3.us-west-004.backblazeb2.com".to_string();
-    let region = Region::Custom {
-        region: region_name,
-        endpoint,
-    };
-
-    let bucket_name = "Blazemaster";
-    let credentials = Credentials::default().unwrap();
-
-    let bucket = Bucket::new(bucket_name, region, credentials)?;
-
-    let results = bucket.list("".to_string(), None).await?;
-
-    for result in results.iter() {
-        for object in result.contents.iter() {
-            println!("{:?}, ", object);
-        } 
-    }
-
-    let (data, code) = bucket.get_object("/monke.gif").await?;
-
-    
-
-    // println!("data = {:?}", data);
-
-    Ok(())
-
 }
---
+++ src/s3support.rs
@@ -0,0 +1,36 @@
+use s3::{creds::Credentials, Bucket, Region};
+
+pub struct Session {
+    bucket: Bucket,
+}
+
+pub async fn connect_to_bucket() -> Result<Session, Box<dyn std::error::Error>>  {
+    let region_name = "us-west-004".to_string();
+    let endpoint = "https://s3.us-west-004.backblazeb2.com".to_string();
+    let region = Region::Custom {
+        region: region_name,
+        endpoint,
+    };
+
+    let bucket_name = "Blazemaster";
+    let credentials = Credentials::default().unwrap();
+
+    let bucket = Bucket::new(bucket_name, region, credentials)?;
+
+    let results = bucket.list("".to_string(), None).await?;
+
+    for result in results.iter() {
+        for object in result.contents.iter() {
+            println!("{:?}, ", object);
+        } 
+    }
+
+    let (data, code) = bucket.get_object("/monke.gif").await?;
+
+    
+
+    // println!("data = {:?}", data);
+
+    Ok(Session { bucket })
+
+}
