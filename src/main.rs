a72cfeee5c1ec4b774a4c50dd311fcd5a37ecd76
amellalalex <github@alexamellal.com>
2022-07-03 15:57:03 -0300

    removed old-s3 from master

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-mod s3support;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let bucket = s3support::connect().await?;
-    s3support::list(&bucket).await?;
-    s3support::download_file(&bucket, "monke.gif", "LEmonke.gif").await?;
-
-    Ok(())
+fn main() {
+    println!("waddup");
 }
